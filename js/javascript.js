2d9a7f058d2f07974c63c8e2fcd5185277f70b11
Your Name <youremail@yourdomain.com>
2022-07-18 10:11:42 -0700

    animate & scroll

--- js/javascript.js
+++ js/javascript.js
@@ -1,22 +1,41 @@
-const addList = document.querySelector('.add-list');
-const addBtn = document.querySelector('.uil-plus');
-const inputField = document.getElementById('input');
+// const addList = document.querySelector('.add-list');
+// const addBtn = document.querySelector('.uil-plus');
+// const inputField = document.getElementById('input');
+
+// addBtn.addEventListener('click', () => {
+//     const createElement = document.createElement('div');
+//     createElement.classList.add('create-div');
+//     createElement.innerText = inputField.value;
+//     addList.appendChild(createElement);
+//     inputField.value = '';
+
+
+//     createElement.addEventListener('click', () => {
+//         createElement.style.textDecoration = 'line-through';
+//     })
+
+//     createElement.addEventListener('dblclick', () => {
+//         addList.removeChild(createElement);  
+//     })
+// });
+
+let addList = document.querySelector('.add-list');
+let addBtn = document.querySelector('.uil-plus');
+let inputField = document.querySelector('#input');
 
 addBtn.addEventListener('click', () => {
-    console.log('hello');
     const createElement = document.createElement('div');
+    createElement.classList.add('animate__animated', 'animate__bounce');
     createElement.classList.add('create-div');
     createElement.innerText = inputField.value;
-    addList.appendChild(createElement);
-    inputField.value = '';
 
+    addList.appendChild(createElement);
 
     createElement.addEventListener('click', () => {
         createElement.style.textDecoration = 'line-through';
-    })
+    });
 
     createElement.addEventListener('dblclick', () => {
-        addList.removeChild(createElement);  
-    })
+        addList.removeChild(createElement);
+    });
 });
-
