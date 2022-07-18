fd0d1b59995df2a4f7cdafdc93aa606a8eab084f
Your Name <youremail@yourdomain.com>
2022-07-18 12:08:07 -0700

    if statement for input

--- js/javascript.js
+++ js/javascript.js
@@ -22,13 +22,21 @@
 let addList = document.querySelector('.add-list');
 let addBtn = document.querySelector('.uil-plus');
 let inputField = document.querySelector('#input');
+let fieldIn = document.querySelector('#fieldIn');
 
 addBtn.addEventListener('click', () => {
     const createElement = document.createElement('div');
     createElement.classList.add('animate__animated', 'animate__slideInDown');
     createElement.classList.add('create-div');
     createElement.innerText = inputField.value;
+    
 
+    if(inputField.value == '') {
+        fieldIn.style.display = 'block';
+        return false;
+    } else {
+        fieldIn.style.display = 'none';
+    }
     addList.appendChild(createElement);
 
     createElement.addEventListener('click', () => {
@@ -39,6 +47,6 @@ addBtn.addEventListener('click', () => {
     createElement.addEventListener('dblclick', () => {
         addList.removeChild(createElement);
 
-
+       
     });
 });
